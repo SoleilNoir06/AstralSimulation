daebea3e9348a585242da5ce3c7f9eb47f130501
Evan Comtesse <evan.comtesse@rpn.ch>
2024-11-12 11:11:29 +0100

    feat: loaded and rendered skybox

---
+++ src/code/3D/Conceptor3D.cs
@@ -0,0 +1,43 @@
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
+using Raylib_cs.Complements;
+using static Raylib_cs.Complements.Raylib;
+using System.Numerics;
+
+namespace Astral_simulation
+{
+    /// <summary>Represents an instance of <see cref="Conceptor3D"/>.</summary>
+    public static class Conceptor3D
+    {
+        // -----------------------------------------------------------
+        // Private instances
+        // -----------------------------------------------------------
+
+        private static Camera3D _camera;
+        private static Skybox _skybox;
+
+        /// <summary>Initializes the 3D environnment of the application.</summary>
+        public static void Init()
+        {
+            _camera = new Camera3D() // Init 3D camera
+            {
+                Position = new Vector3(1f, 1f, 1f),
+                Target = Vector3.UnitX,
+                Up = Vector3.UnitY,
+                FovY = 45f,
+                Projection = CameraProjection.Perspective
+            };
+            _skybox = LoadSkybox("assets/shaders/skyboxes/HDR_blue_nebulae-1.hdr");
+        }
+
+        /// <summary>Draws the 3D environnement of the application.</summary>
+        public static void Draw()
+        {
+            BeginMode3D(_camera);
+
+            DrawSkybox(_skybox);
+
+            EndMode3D();
+        }
+    }
+}
---
+++ src/code/3D/ShaderCenter.cs
@@ -0,0 +1,37 @@
+﻿using static Raylib_cs.Raylib;
+using Raylib_cs;
+
+namespace Astral_simulation
+{
+    /// <summary>Represents an instance of <see cref="ShaderCenter"/>.</summary>
+    public static class ShaderCenter
+    {
+        /// <summary>Cubemap loading shader.</summary>
+        public static Shader CubemapShader;
+
+        /// <summary>Skybox rendering shader.</summary>
+        public static Shader SkyboxShader;
+
+        /// <summary>Inits the shader center by loading the shaders of the application.</summary>
+        public static void Init()
+        {
+            // Load skybox shader
+            SkyboxShader = LoadShader("assets/shaders/skybox.vs", "assets/shaders/skybox.fs");
+            SetShaderValue(SkyboxShader, GetShaderLocation(SkyboxShader, "environmentMap"), (int)MaterialMapIndex.Cubemap, ShaderUniformDataType.Int);
+            SetShaderValue(SkyboxShader, GetShaderLocation(SkyboxShader, "doGamma"), 1, ShaderUniformDataType.Int);
+            SetShaderValue(SkyboxShader, GetShaderLocation(SkyboxShader, "vflipped"), 1, ShaderUniformDataType.Int);
+
+            // Load cubemap shader
+            CubemapShader = LoadShader("assets/shaders/cubemap.vs", "assets/shaders/cubemap.fs");
+            SetShaderValue(CubemapShader, GetShaderLocation(CubemapShader, "equirectangularMap"), 0, ShaderUniformDataType.Int);
+        }
+
+        /// <summary>Closes the shader center by unloading every program shader from the vRAM.</summary>
+        public static void Close()
+        {
+            // Unload shader programs from vRAM
+            UnloadShader(CubemapShader);
+            UnloadShader(SkyboxShader);
+        }
+    }
+}
