6a5dde25c24193e794ce95153bb12ab7ddcda85d
Evan Comtesse <evan.comtesse@rpn.ch>
2024-11-12 08:31:24 +0100

    feat: created project template

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,29 @@
 ﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
+using System.Numerics;
+
+namespace Astral_simulation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InitWindow(0, 0, "Astra Simulation");
+            SetWindowState(ConfigFlags.ResizableWindow);
+            SetWindowState(ConfigFlags.MaximizedWindow);
+
+            while (!WindowShouldClose())
+            {
+                BeginDrawing();
+
+                ClearBackground(Color.White);
+
+                EndDrawing();
+            }
+
+            CloseWindow();
+
+            // Unloading
+        }
+    }
+}
