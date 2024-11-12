b00660771f0afa245902732f9af3822be3a249d5
Ethan Roth <ethan.roth@rpn.ch>
2024-11-12 09:46:17 +0100

    feat: Creation classes Telluric and Gaseous

---
+++ src/code/Objects/Gaseous.cs
@@ -0,0 +1,18 @@
+﻿using Astral_simulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace astral_simulation
+{
+    public class Gaseous : AstralObject
+    {
+        public Gaseous(long mass, long radius, float orbitPeriod, float rotationPeriod) : base(mass, radius, orbitPeriod, rotationPeriod) 
+        { 
+
+        }
+
+    }
+}
---
+++ src/code/Objects/Telluric.cs
@@ -0,0 +1,31 @@
+﻿using Astral_simulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace astral_simulation
+{
+    public enum Elements
+    {
+        Silicate,
+        Limestone,
+        Sand, 
+        Iron,
+        Clay,
+        Silt,
+        Water,
+        Lava,
+        Dirt,
+        Aluminum,
+        Rocks
+    }
+    public class Telluric : AstralObject
+    {
+        public Telluric(long mass, long radius, float orbitPeriod, float rotationPeriod) : base (mass, radius, orbitPeriod, rotationPeriod)
+        {
+
+        }
+    }
+}
