8be9b5b93f8bb0341a7cd0745ed0fd05dc33cb6d
Evan Comtesse <Evan.Comtesse@rpn.ch>
2024-12-10 10:41:05 +0100

    feat: Update RayGUI_cs

--- src/code/Interface/Conceptor2D.cs
+++ src/code/Interface/Conceptor2D.cs
@@ -24,17 +24,22 @@ namespace Astral_simulation
 
         public static void DisplayObject(AstralObject obj)
         {
-            RayGUI.SetDefaultFontSize(30);
+            SetDefaultFontSize(30);
             Components.Clear();
             Container c = new Container(10, 10, 520, GetScreenHeight() - 20);
             c.BaseColor = new Color(22, 22, 22, 20);
             Components.Add(c);
-            Components.Add(new Textbox(20, 20, 500, 50, $"{obj.Name}"));
-            Components.Add(new Textbox(20, 70, 500, 50, $"Type: {obj.Type}"));
-            Components.Add(new Textbox(20, 120, 500, 50, $"Mass: {obj.Mass}e24 kg"));
-            Components.Add(new Textbox(20, 170, 500, 50, $"Radius: {obj.Radius * 150000f}km"));
-            Components.Add(new Textbox(20, 220, 500, 50, $"Volume: {obj.Volume}km^3"));
-            Components.Add(new Textbox(20, 270, 500, 50, $"Distance from sun : {Physics.ComputeRadialDistance(obj.Position) * 15000000}km"));
+            Components.Add(new Label(20, 20, 500, 50, $"{obj.Name}"));
+            Components.Add(new Label(20, 70, 500, 50, $"Type: {obj.Type}"));
+            Components.Add(new Label(20, 120, 500, 50, $"Mass: {obj.Mass}e24 kg"));
+            Components.Add(new Label(20, 170, 500, 50, $"Radius: {obj.Radius * 150000f}km"));
+            Components.Add(new Label(20, 220, 500, 50, $"Volume: {obj.Volume}km^3"));
+            Components.Add(new Label(20, 270, 500, 50, $"Distance from sun : {Physics.ComputeRadialDistance(obj.Position) * 15000000}km"));
+
+            /* Panel p = new Panel(20, 300, 0, LoadTexture("assets/textures/splash.png"));
+            p.MaxHeight = 200;
+            p.MaxWidth = 200;
+            Components.Add(p); */
         }
     }
 }
